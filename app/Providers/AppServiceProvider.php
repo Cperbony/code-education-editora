77196ef4330d82dd298b19515168ada0a0be505b
Claudinei Perboni <cperbony@gmail.com>
2017-11-06 14:33:31 -0200

    Regitro das facades de config.app das chaves aliases no AppServiceProvider para eliminar o erro de toString no PHPStorm

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,30 @@ namespace CodePub\Providers;
 
 use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
 
+use Bootstrapper\Facades\Accordion;
+use Bootstrapper\Facades\Alert;
+use Bootstrapper\Facades\Badge;
+use Bootstrapper\Facades\Breadcrumb;
+use Bootstrapper\Facades\Button;
+use Bootstrapper\Facades\ButtonGroup;
+use Bootstrapper\Facades\Carousel;
+use Bootstrapper\Facades\ControlGroup;
+use Bootstrapper\Facades\DropdownButton;
+use Bootstrapper\Facades\Helpers;
+use Bootstrapper\Facades\Icon;
+use Bootstrapper\Facades\Image;
+use Bootstrapper\Facades\InputGroup;
+use Bootstrapper\Facades\Label;
+use Bootstrapper\Facades\MediaObject;
+use Bootstrapper\Facades\Modal;
+use Bootstrapper\Facades\Navbar;
+use Bootstrapper\Facades\Navigation;
+use Bootstrapper\Facades\Panel;
+use Bootstrapper\Facades\ProgressBar;
+use Bootstrapper\Facades\Tabbable;
+use Bootstrapper\Facades\Table;
+use Bootstrapper\Facades\Thumbnail;
+use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -59,6 +83,33 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $loader = AliasLoader::getInstance();
+        $loader->alias('Accordion', Accordion::class);
+        $loader->alias('Alert', Alert::class);
+        $loader->alias('Badge', Badge::class);
+        $loader->alias('Badge', Badge::class);
+        $loader->alias('Breadcrumb', Breadcrumb::class);
+        $loader->alias('Button', Button::class);
+        $loader->alias('ButtonGroup', ButtonGroup::class);
+        $loader->alias('Carousel', Carousel::class);
+        $loader->alias('ControlGroup', ControlGroup::class);
+        $loader->alias('DropdownButton', DropdownButton::class);
+        $loader->alias('Helpers', Helpers::class);
+        $loader->alias('Icon', Icon::class);
+        $loader->alias('InputGroup', InputGroup::class);
+        $loader->alias('Image', Image::class);
+        $loader->alias('Label', Label::class);
+        $loader->alias('MediaObject', MediaObject::class);
+        $loader->alias('Modal', Modal::class);
+        $loader->alias('Navbar', Navbar::class);
+        $loader->alias('Navigation', Navigation::class);
+        $loader->alias('Panel', Panel::class);
+        $loader->alias('ProgressBar', ProgressBar::class);
+        $loader->alias('Tabbable', Tabbable::class);
+        $loader->alias('Table', Table::class);
+        $loader->alias('Thumbnail', Thumbnail::class);
+//        $loader->alias('Form', \Bootstrapper\Facades\Form::class);
+
         if ($this->app->environment() !== 'production') {
             $this->app->register(IdeHelperServiceProvider::class);
         }
